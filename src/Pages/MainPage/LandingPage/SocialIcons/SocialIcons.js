fce5eb2b9a4d81938c360817ad1da1e5b1815d36
Karolis Kimtys <karolis_k_7@hotmail.com>
2021-09-14 15:56:19 +0200

    fixes

--- src/Pages/MainPage/LandingPage/SocialIcons/SocialIcons.js
+++ src/Pages/MainPage/LandingPage/SocialIcons/SocialIcons.js
@@ -1,4 +1,7 @@
 import styles from './Social.module.scss';
+import ReactTooltip from 'react-tooltip';
+
+import { useContextProvider } from '../../../../components/ContextProvider/Provider';
 
 import Profile from './Profile/Profile';
 import SendMail from './SendMail/SendMail';
@@ -6,12 +9,30 @@ import GitHub from './GitHub/GitHub';
 import LinkedIn from './LinkedIn/LinkedIn';
 
 export default function SocialIcons() {
+  const { isDarkTheme } = useContextProvider();
+
   return (
     <div className={styles.Social}>
-      <Profile />
-      <SendMail />
-      <GitHub />
-      <LinkedIn />
+      <a href data-tip="Resume">
+        <Profile />
+      </a>
+      <a href data-tip="E-Mail Me">
+        <SendMail />
+      </a>
+      <a href data-tip="GitHub">
+        <GitHub />
+      </a>
+      <a href data-tip="LinkedIn">
+        <LinkedIn />
+      </a>
+
+      <ReactTooltip
+        place="bottom"
+        type="info"
+        effect="solid"
+        textColor={isDarkTheme ? 'rgb(34, 40, 49)' : 'rgb(242, 163, 101)'}
+        backgroundColor={isDarkTheme ? 'rgb(242, 163, 101)' : 'rgb(34, 40, 49)'}
+      />
     </div>
   );
 }
